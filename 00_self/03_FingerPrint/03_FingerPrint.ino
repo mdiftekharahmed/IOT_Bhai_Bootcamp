e15c3b33820e34171572262e1a9f5d6605920ed2
mdiftekharahmed <mdiftekharahmed1449@gmail.com>
2026-04-28 01:31:55 +0600

    added week 9

--- 00_self/03_FingerPrint/03_FingerPrint.ino
+++ 00_self/03_FingerPrint/03_FingerPrint.ino
@@ -20,51 +20,7 @@ void setup(){
 }
 
 void loop(){
-    //check finger availablity
-    if(finger.getImage()!= FINGERPRINT_OK){
-      return;
-    }
-
-    //convert to characteristics
-    finger.image2Tz(1);
-
-    //search against stored templates
-    uint8_t p =finger.fingerSearch();
-    if(p==FINGERPRINT_OK){
-      Serial.print("Found ID #");
-      Serial.print(finger.fingerID);
-      Serial.print(" with confidence: ");
-      Serial.println(finger.confidence);
-    }
-    else{
-      Serial.println("No Match");
-    }
-
-    delay(1000);
+    
 
 }
-
-
-
-void enrollFingerprint(uint8_t id){
-  Serial.print("Enrolling ID #");
-  Serial.println(id);
-  while(finger.getImage()!=FINGERPRINT_OK);
-  finger.image2Tz(1);
-  Serial.println("Remove Finger");
-  delay(2000);
-  while(finger.getImage()!=FINGERPRINT_OK);
-  finger.image2Tz(2);
-  int ok = finger.createModel();
-  if(ok==FINGERPRINT_OK){
-    finger.storeModel(id);
-    Serial.println("Stored");
-  }
-  else{
-    Serial.println("Failed to create Model");
-  }
-}
-
-
-
 
